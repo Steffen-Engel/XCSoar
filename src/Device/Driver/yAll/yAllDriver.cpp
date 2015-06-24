6df92d4d4b5a6f1257fa02713a3927fcd5dec1a4
Steffen Engel <Steffen.Engel@nutrimat.de>
2015-06-24 21:04:53 +0200

    Added yAll device driver and infoboxes with data of yAll Device

---
+++ src/Device/Driver/yAll/yAllDriver.cpp
@@ -0,0 +1,141 @@
+/*
+Copyright_License {
+
+  XCSoar Glide Computer - http://www.xcsoar.org/
+  Copyright (C) 2000-2013 The XCSoar Project
+  A detailed list of copyright holders can be found in the file "AUTHORS".
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+}
+*/
+
+#include "Device/Driver/yAll/Internal.hpp"
+#include "Device/Driver/yAll/yAllDriver.hpp"
+#include "Device/Driver.hpp"
+#include "Device/Port/Port.hpp"
+#include "NMEA/Info.hpp"
+#include "NMEA/InputLine.hpp"
+#include "Atmosphere/Temperature.hpp"
+#include "LogFile.hpp"
+#include "InfoBoxes/InfoBoxManager.hpp"
+
+#include "Operation/Operation.hpp"
+#include "yAllProtocol.h"
+#include "yAll.h"
+
+#include <tchar.h>
+
+#include <stdint.h>
+
+#define DATA_TIME 50
+
+
+
+
+
+
+bool
+yAllDevice::DataReceived(const void *_data, size_t length,
+                          struct NMEAInfo &info){
+//  LogDebug(_T("yAll data %d"), (int)length);
+
+  const char *data = (const char *)_data;
+  for (int count = 0; count < (int)length; count++)
+  {
+    yAll.parse(data[count], info);
+  }
+
+  return true;
+}
+
+
+
+bool
+yAllDevice::EnableNMEA(OperationEnvironment &env)
+{
+  LogDebug(_T("yAll EnableNMEA"));
+
+  //env.Sleep(5000);
+  //yAll.sendRequest(MSP_RESET);
+  //env.Sleep(500);
+  //yAll.sendRequest(MSP_LOGGER_SET, (uint32_t)DATA_TIME);
+  yAll.sendRequest(MSP_IDENT);
+
+  return true;
+}
+
+
+void
+yAllDevice::OnSysTicker(){
+  //LogDebug(_T("yAll SysTicker"));
+  yAll.sendRequest(MSP_LOGGER_SET, (uint32_t)DATA_TIME);
+}
+
+
+void
+yAllDevice::LinkTimeout(){
+  LogDebug(_T("yAll LinkTimeout"));
+  yAll.sendRequest(MSP_LOGGER_SET, (uint32_t)DATA_TIME);
+}
+
+
+void
+yAllDevice::GetInfo(tIdent &Ident)
+{
+  Ident = yAll.Ident;
+};
+
+
+void
+yAllDevice::Restart()
+{
+  yAll.sendRequest(MSP_RESET);
+}
+
+
+void
+yAllDevice::CalibrateAcc()
+{
+  yAll.sendRequest(MSP_ACC_CALIBRATION);
+}
+
+
+void
+yAllDevice::CalibrateIAS()
+{
+  yAll.sendRequest(MSP_IAS_CALIBRATION);
+}
+
+
+void
+yAllDevice::CalibrateMag()
+{
+  yAll.sendRequest(MSP_MAG_CALIBRATION);
+}
+
+
+
+static Device *
+yAllCreateOnPort(const DeviceConfig &config, Port &com_port)
+{
+  return new yAllDevice(com_port);
+}
+
+const struct DeviceRegister yall_driver = {
+  _T("yAll"),
+  _T("yAll"),
+  DeviceRegister::RAW_GPS_DATA | DeviceRegister::MANAGE,
+  yAllCreateOnPort,
+};
