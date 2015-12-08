9df75ed1430e50ac0d8baca81695ae8d7a8a02ce
Steffen Engel <Steffen.Engel@nutrimat.de>
2015-12-08 22:45:52 +0100

    New device driver for Weatherstations Parsing standard NMEA sentences WIMWV and WIMDA

--- src/Device/Register.cpp
+++ src/Device/Register.cpp
@@ -57,6 +57,7 @@ Copyright_License {
 #include "Device/Driver/ATR833.hpp"
 #include "Device/Driver/XCTracer.hpp"
 #include "Device/Driver/KRT2.hpp"
+#include "Device/Driver/NMEA_WX.hpp"
 #include "Util/Macros.hpp"
 #include "Util/StringAPI.hxx"
 
@@ -100,6 +101,7 @@ static const struct DeviceRegister *const driver_list[] = {
   &krt2_driver,
   &atr833_driver,
   &xctracer_driver,
+  &nmea_wx_driver,
   nullptr
 };
 
