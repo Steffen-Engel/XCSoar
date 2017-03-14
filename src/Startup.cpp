Steffen-Engel/XCSoar

commit 384abeaa24964994b1e674a02818f6ac10cce2d4

Startup: delete main window instance in Shutdown()
This should fix #3679.

src/Startup.cpp
@@ -668,6 +668,7 @@ Shutdown()
 
   LogFormat("Close Windows - main");
   main_window->Destroy();
+  delete main_window;
 
   CloseLanguageFile();
 

commit 029ea097f1eeef3b59bf6428f60721f27470a09e

fix to #3679 in 384abeaa24964994b1e674a02818f6ac10cce2d4 introduced a crash in Android due to access to a deleted class. Classpointer set to nullptr after deleting fixes this.

src/Startup.cpp
@@ -665,6 +665,7 @@ Shutdown()
   LogFormat("Close Windows - main");
   main_window->Destroy();
   delete main_window;
+  CommonInterface::main_window = nullptr;
 
   CloseLanguageFile();
 