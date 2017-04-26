Steffen-Engel/XCSoar

commit 25b1df2d6c702d7d2eb8d2d51abc47367e21cdda

Airspaces: add Query*() returning an iterator range
Switch from visitor pattern to iterators.  This is easier to use and
results in less runtime bloat.

src/Engine/Airspace/Airspaces.cpp
@@ -43,26 +43,18 @@ struct AirspacePredicateAdapter {
   }
 };
 
-void
-Airspaces::VisitWithinRange(const GeoPoint &location, fixed range,
-                            AirspaceVisitor &visitor,
-                            const AirspacePredicate &predicate) const
+Airspaces::const_iterator_range
+Airspaces::QueryWithinRange(const GeoPoint &location, fixed range) const
 {
   if (IsEmpty())
     // nothing to do
-    return;
+    return {airspace_tree.qend(), airspace_tree.qend()};
 
   const auto flat_location = task_projection.ProjectInteger(location);
   int projected_range = task_projection.ProjectRangeInteger(location, range);
   const FlatBoundingBox box(flat_location, projected_range);
 
-  const auto _begin =
-    airspace_tree.qbegin(bgi::intersects(box) &&
-                         bgi::satisfies(AirspacePredicateAdapter{predicate}));
-  const auto _end = airspace_tree.qend();
-
-  for (auto i = _begin; i != _end; ++i)
-    visitor.Visit(i->GetAirspace());
+  return {airspace_tree.qbegin(bgi::intersects(box)), airspace_tree.qend()};
 }
 
 class IntersectingAirspaceVisitorAdapter {
@@ -347,12 +339,12 @@ Airspaces::SynchroniseInRange(const Airspaces &master,
   return true;
 }
 
-void
-Airspaces::VisitInside(const GeoPoint &loc, AirspaceVisitor &visitor) const
+Airspaces::const_iterator_range
+Airspaces::QueryInside(const GeoPoint &loc) const
 {
   if (IsEmpty())
     // nothing to do
-    return;
+    return {airspace_tree.qend(), airspace_tree.qend()};
 
   const auto flat_location = task_projection.ProjectInteger(loc);
   const FlatBoundingBox box(flat_location, flat_location);
@@ -362,8 +354,6 @@ Airspaces::VisitInside(const GeoPoint &loc, AirspaceVisitor &visitor) const
                          bgi::satisfies([&loc](const Airspace &as){
                              return as.IsInside(loc);
                            }));
-  const auto _end = airspace_tree.qend();
 
-  for (auto i = _begin; i != _end; ++i)
-    visitor.Visit(i->GetAirspace());
+  return {_begin, airspace_tree.qend()};
 }

src/Engine/Airspace/Airspaces.hpp
@@ -145,18 +145,19 @@ public:
    */
   void SetActivity(const AirspaceActivity mask);
 
+  const_iterator_range QueryAll() const {
+    return {begin(), end()};
+  }
+
   /**
-   * Call visitor class on airspaces within range of location.
-   * Note that the visitor is not instantiated separately for each match
+   * Query airspaces within range of location.
    *
    * @param loc location of origin of search
    * @param range distance in meters of search radius
-   * @param visitor visitor class to call on airspaces within range
    */
-  void VisitWithinRange(const GeoPoint &location, fixed range,
-                        AirspaceVisitor &visitor,
-                        const AirspacePredicate &predicate =
-                              AirspacePredicate::always_true) const;
+  gcc_pure
+  const_iterator_range QueryWithinRange(const GeoPoint &location,
+                                        fixed range) const;
 
   /**
    * Call visitor class on airspaces intersected by vector.
@@ -170,13 +171,12 @@ public:
                          AirspaceIntersectionVisitor &visitor) const;
 
   /**
-   * Call visitor class on airspaces this location is inside
-   * Note that the visitor is not instantiated separately for each match
+   * Query airspaces this location is inside.
    *
    * @param loc location of origin of search
-   * @param visitor visitor class to call on airspaces
    */
-  void VisitInside(const GeoPoint &location, AirspaceVisitor &visitor) const;
+  gcc_pure
+  const_iterator_range QueryInside(const GeoPoint &location) const;
 
   /**
    * Search for airspaces within range of the aircraft.

src/Engine/Airspace/Minimum.hpp
@@ -25,7 +25,6 @@ Copyright_License {
 #define AIRSPACE_MINIMUM_HPP
 
 #include "Airspaces.hpp"
-#include "AirspaceVisitor.hpp"
 
 template<class Func,
          typename Result=decltype(((Func *)nullptr)->operator()(*(const AbstractAirspace *)nullptr)),
@@ -37,22 +36,15 @@ FindMinimum(const Airspaces &airspaces, const GeoPoint &location, fixed range,
             Func &&func,
             Cmp &&cmp=Cmp())
 {
-  struct FindMinimumVisitor final : AirspaceVisitor, Func, Cmp {
-    Result minimum;
+  Result minimum;
+  for (const auto &i : airspaces.QueryWithinRange(location, range)) {
+    const AbstractAirspace &aa = i.GetAirspace();
+    Result result = func(aa);
+    if (cmp(result, minimum))
+      minimum = result;
+  }
 
-    FindMinimumVisitor(Func &&_func, Cmp &&_cmp)
-      :Func(std::move(_func)), Cmp(std::move(_cmp)) {}
-
-    void Visit(const AbstractAirspace &aa) override {
-      Result result = Func::operator()(aa);
-      if (Cmp::operator()(result, minimum))
-        minimum = result;
-    }
-  };
-
-  FindMinimumVisitor visitor(std::move(func), std::move(cmp));
-  airspaces.VisitWithinRange(location, range, visitor, predicate);
-  return visitor.minimum;
+  return minimum;
 }
 
 #endif

src/Airspace/NearestAirspace.cpp
@@ -26,7 +26,6 @@ Copyright_License {
 #include "Airspace/ActivePredicate.hpp"
 #include "Engine/Airspace/Airspaces.hpp"
 #include "Engine/Airspace/AbstractAirspace.hpp"
-#include "Engine/Airspace/AirspaceVisitor.hpp"
 #include "Engine/Airspace/Predicate/AirspacePredicate.hpp"
 #include "Engine/Airspace/Predicate/AirspacePredicateHeightRange.hpp"
 #include "Engine/Airspace/Predicate/OutsideAirspacePredicate.hpp"
@@ -102,33 +101,34 @@ NearestAirspace::FindHorizontal(const MoreData &basic,
   }
 }
 
-/**
- * This AirspaceVisitor extracts the vertical nearest airspace
- */
-class VerticalAirspaceVisitor : public AirspaceVisitor {
+gcc_pure
+NearestAirspace
+NearestAirspace::FindVertical(const MoreData &basic,
+                      const DerivedInfo &calculated,
+                      const ProtectedAirspaceWarningManager &airspace_warnings,
+                      const Airspaces &airspace_database)
+{
+  if (!basic.location_available ||
+      (!basic.baro_altitude_available && !basic.gps_altitude_available))
+    /* can't check for airspaces without a GPS fix and altitude
+       value */
+    return NearestAirspace();
+
+  /* find the nearest airspace */
+
   AltitudeState altitude;
+  altitude.altitude = basic.nav_altitude;
+  altitude.altitude_agl = calculated.altitude_agl;
 
-  const AbstractAirspace *nearest;
-  fixed nearest_delta;
-  const ActiveAirspacePredicate active_predicate;
+  const AbstractAirspace *nearest = nullptr;
+  double nearest_delta = 100000;
+  const ActiveAirspacePredicate active_predicate(&airspace_warnings);
 
-public:
-  VerticalAirspaceVisitor(const MoreData &basic,
-                          const DerivedInfo &calculated,
-                          const ProtectedAirspaceWarningManager &airspace_warnings)
-    :nearest(nullptr),
-     nearest_delta(100000),
-     active_predicate(&airspace_warnings)
-  {
-    assert(basic.baro_altitude_available || basic.gps_altitude_available);
-    altitude.altitude = basic.nav_altitude;
-    altitude.altitude_agl = calculated.altitude_agl;
-  }
+  for (const auto &i : airspace_database.QueryInside(basic.location)) {
+    const AbstractAirspace &airspace = i.GetAirspace();
 
-protected:
-  void Check(const AbstractAirspace &airspace) {
     if (!active_predicate(airspace))
-      return;
+      continue;
 
     /* check delta below */
     auto base = airspace.GetBase().GetAltitude(altitude);
@@ -147,38 +147,8 @@ protected:
     }
   }
 
-  void Visit(const AbstractAirspace &as) override {
-    Check(as);
-  }
-
-public:
-  const AbstractAirspace *GetNearest() const {
-    return nearest;
-  }
-
-  fixed GetNearestDelta() const {
-    return nearest_delta;
-  }
-};
-
-gcc_pure
-NearestAirspace
-NearestAirspace::FindVertical(const MoreData &basic,
-                      const DerivedInfo &calculated,
-                      const ProtectedAirspaceWarningManager &airspace_warnings,
-                      const Airspaces &airspace_database)
-{
-  if (!basic.location_available ||
-      (!basic.baro_altitude_available && !basic.gps_altitude_available))
-    /* can't check for airspaces without a GPS fix and altitude
-       value */
+  if (nearest == nullptr)
     return NearestAirspace();
 
-  /* find the nearest airspace */
-  VerticalAirspaceVisitor visitor(basic, calculated, airspace_warnings);
-  airspace_database.VisitInside(basic.location, visitor);
-  if (visitor.GetNearest() == nullptr)
-    return NearestAirspace();
-
-  return NearestAirspace(*visitor.GetNearest(), visitor.GetNearestDelta());
+  return NearestAirspace(*nearest, nearest_delta);
 }

src/Engine/Airspace/AirspaceSorter.cpp
@@ -1,7 +1,6 @@
 #include "AirspaceSorter.hpp"
 #include "Airspace/Airspaces.hpp"
 #include "AbstractAirspace.hpp"
-#include "AirspaceVisitor.hpp"
 #include "Geo/GeoVector.hpp"
 #include "Util/StringAPI.hxx"
 
@@ -70,15 +69,6 @@ public:
   }
 };
 
-class AirspaceFilterVisitor final : public AirspaceVisitor {
-public:
-  AirspaceSelectInfoVector result;
-
-  void Visit(const AbstractAirspace &as) override {
-    result.emplace_back(as);
-  }
-};
-
 static void
 SortByDistance(AirspaceSelectInfoVector &vec, const GeoPoint &location,
                const FlatProjection &projection)
@@ -110,19 +100,19 @@ FilterAirspaces(const Airspaces &airspaces, const GeoPoint &location,
 {
   const AirspaceFilterPredicate predicate(location, airspaces.GetProjection(),
                                           filter);
-  AirspaceFilterVisitor visitor;
+  AirspaceSelectInfoVector result;
 
-  if (!negative(filter.distance))
-    airspaces.VisitWithinRange(location, filter.distance, visitor, predicate);
-  else
-    for (const auto &i : airspaces)
-      if (predicate(i.GetAirspace()))
-        visitor.Visit(i.GetAirspace());
+  auto range = negative(filter.distance)
+    ? airspaces.QueryAll()
+    : airspaces.QueryWithinRange(location, filter.distance);
+  for (const auto &i : range)
+    if (predicate(i.GetAirspace()))
+      result.emplace_back(i.GetAirspace());
 
   if (filter.direction.IsNegative() && negative(filter.distance))
-    SortByName(visitor.result);
+    SortByName(result);
   else
-    SortByDistance(visitor.result, location, airspaces.GetProjection());
+    SortByDistance(result, location, airspaces.GetProjection());
 
-  return visitor.result;
+  return result;
 }

src/Engine/Airspace/AirspaceWarningManager.cpp
@@ -298,7 +298,11 @@ AirspaceWarningManager::UpdatePredicted(const AircraftState& state,
   airspaces.VisitIntersecting(state.location, location_predicted, visitor);
 
   visitor.SetMode(true);
-  airspaces.VisitInside(state.location, visitor);
+
+  for (const auto &i : airspaces.QueryInside(state.location)) {
+    const AbstractAirspace &airspace = i.GetAirspace();
+    visitor.Visit(airspace);
+  }
 
   return visitor.Found();
 }
@@ -377,50 +381,6 @@ AirspaceWarningManager::UpdateGlide(const AircraftState &state,
                           AirspaceWarning::WARNING_GLIDE, prediction_time_glide);
 }
 
-class InsideWarningVisitor final : public AirspaceVisitor {
-  AirspaceWarningManager &wm;
-  const AircraftState &state;
-  const GlidePolar &glide_polar;
-
-  bool found = false;
-
-public:
-  InsideWarningVisitor(AirspaceWarningManager &_wm,
-                       const AircraftState &_state,
-                       const GlidePolar &_glide_polar)
-    :wm(_wm), state(_state), glide_polar(_glide_polar) {}
-
-  bool WasFound() const {
-    return found;
-  }
-
-  void Visit(const AbstractAirspace &airspace) override {
-    const AltitudeState &altitude = state;
-
-    if (// ignore inactive airspaces
-        !airspace.IsActive() ||
-        !wm.GetConfig().IsClassEnabled(airspace.GetType()) ||
-        !airspace.Inside(altitude))
-      return;
-
-    AirspaceWarning *warning = wm.GetWarningPtr(airspace);
-
-    if (warning == nullptr ||
-        warning->IsStateAccepted(AirspaceWarning::WARNING_INSIDE)) {
-      GeoPoint c = airspace.ClosestPoint(state.location, wm.GetProjection());
-      const AirspaceAircraftPerformance perf_glide(glide_polar);
-      AirspaceInterceptSolution solution;
-      airspace.Intercept(state, c, wm.GetProjection(), perf_glide, solution);
-
-      if (warning == nullptr)
-        warning = wm.GetNewWarningPtr(airspace);
-
-      warning->UpdateSolution(AirspaceWarning::WARNING_INSIDE, solution);
-      found = true;
-    }
-  }
-};
-
 bool
 AirspaceWarningManager::UpdateInside(const AircraftState& state,
                                      const GlidePolar &glide_polar)
@@ -428,9 +388,36 @@ AirspaceWarningManager::UpdateInside(const AircraftState& state,
   if (!glide_polar.IsValid())
     return false;
 
-  InsideWarningVisitor visitor(*this, state, glide_polar);
-  airspaces.VisitInside(state.location, visitor);
-  return visitor.WasFound();
+  bool found = false;
+
+  for (const auto &i : airspaces.QueryInside(state.location)) {
+    const AbstractAirspace &airspace = i.GetAirspace();
+
+    const AltitudeState &altitude = state;
+    if (// ignore inactive airspaces
+        !airspace.IsActive() ||
+        !config.IsClassEnabled(airspace.GetType()) ||
+        !airspace.Inside(altitude))
+      continue;
+
+    AirspaceWarning *warning = GetWarningPtr(airspace);
+
+    if (warning == nullptr ||
+        warning->IsStateAccepted(AirspaceWarning::WARNING_INSIDE)) {
+      GeoPoint c = airspace.ClosestPoint(state.location, GetProjection());
+      const AirspaceAircraftPerformance perf_glide(glide_polar);
+      AirspaceInterceptSolution solution;
+      airspace.Intercept(state, c, GetProjection(), perf_glide, solution);
+
+      if (warning == nullptr)
+        warning = GetNewWarningPtr(airspace);
+
+      warning->UpdateSolution(AirspaceWarning::WARNING_INSIDE, solution);
+      found = true;
+    }
+  }
+
+  return found;
 }
 
 void

src/Engine/Airspace/AirspacesInterface.hpp
@@ -29,6 +29,7 @@
 #include <boost/geometry/index/rtree.hpp>
 #include <boost/geometry/geometries/register/point.hpp>
 #include <boost/geometry/geometries/register/box.hpp>
+#include <boost/range/iterator_range_core.hpp>
 
 #include <assert.h>
 
@@ -117,6 +118,8 @@ public:
                                         AirspaceIndexable> AirspaceTree;
 
   typedef AirspaceTree::const_query_iterator const_iterator;
+
+  typedef boost::iterator_range<const_iterator> const_iterator_range;
 };
 
 #endif

src/Engine/Route/AirspaceRoute.cpp
@@ -79,23 +79,6 @@ public:
   }
 };
 
-
-class AirspaceInsideOtherVisitor final : public AirspaceVisitor {
-  const AbstractAirspace *m_found;
-
-public:
-  AirspaceInsideOtherVisitor():m_found(nullptr) {};
-
-  const AbstractAirspace *GetFound() const {
-    return m_found;
-  }
-
-protected:
-  void Visit(const AbstractAirspace &as) override {
-    m_found = &as;
-  }
-};
-
 AirspaceRoute::RouteAirspaceIntersection
 AirspaceRoute::FirstIntersecting(const RouteLink &e) const
 {
@@ -111,10 +94,12 @@ AirspaceRoute::FirstIntersecting(const RouteLink &e) const
 const AbstractAirspace *
 AirspaceRoute::InsideOthers(const AGeoPoint &origin) const
 {
-  AirspaceInsideOtherVisitor visitor;
-  m_airspaces.VisitWithinRange(origin, 1, visitor);
   ++count_airspace;
-  return visitor.GetFound();
+
+  for (const auto &i : m_airspaces.QueryWithinRange(origin, 1))
+    return &i.GetAirspace();
+
+  return nullptr;
 }
 
 

commit d9a3e4b50852b5cd3825690d61ff1ac294aaa3ca

fix #3817 Nearest Airspace was not anymore the nearest airspace in actual altitude, but then nearest airspace in any height. Fixed by returning to old code for nearest horizontal airspace before bug introduced by 25b1df2d6c702d7d2eb8d2d51abc47367e21cdda

src/Engine/Airspace/Airspaces.cpp
@@ -30,6 +30,37 @@
 
 namespace bgi = boost::geometry::index;
 
+struct AirspacePredicateAdapter {
+  const AirspacePredicate &predicate;
+
+  bool operator()(const Airspace &as) const {
+    return predicate(as.GetAirspace());
+  }
+};
+
+
+void
+Airspaces::VisitWithinRange(const GeoPoint &location, double range,
+                            AirspaceVisitor &visitor,
+                            const AirspacePredicate &predicate) const
+{
+  if (IsEmpty())
+    // nothing to do
+    return;
+
+  const auto flat_location = task_projection.ProjectInteger(location);
+  int projected_range = task_projection.ProjectRangeInteger(location, range);
+  const FlatBoundingBox box(flat_location, projected_range);
+
+  const auto _begin =
+    airspace_tree.qbegin(bgi::intersects(box) &&
+                         bgi::satisfies(AirspacePredicateAdapter{predicate}));
+  const auto _end = airspace_tree.qend();
+
+  for (auto i = _begin; i != _end; ++i)
+    visitor.Visit(i->GetAirspace());
+}
+
 Airspaces::const_iterator_range
 Airspaces::QueryWithinRange(const GeoPoint &location, double range) const
 {

src/Engine/Airspace/Airspaces.hpp
@@ -25,6 +25,7 @@
 
 #include "AirspacesInterface.hpp"
 #include "AirspaceActivity.hpp"
+#include "Predicate/AirspacePredicate.hpp"
 #include "Util/Serial.hpp"
 #include "Geo/Flat/TaskProjection.hpp"
 #include "Atmosphere/Pressure.hpp"
@@ -33,6 +34,7 @@
 #include <deque>
 
 class RasterTerrain;
+class AirspaceVisitor;
 class AirspaceIntersectionVisitor;
 class AirspacePredicate;
 
@@ -143,6 +145,19 @@ public:
    * @param days Mask of activity (a particular or range of days matching this day)
    */
   void SetActivity(const AirspaceActivity mask);
+
+  /**
+   * Call visitor class on airspaces within range of location.
+   * Note that the visitor is not instantiated separately for each match
+   *
+   * @param loc location of origin of search
+   * @param range distance in meters of search radius
+   * @param visitor visitor class to call on airspaces within range
+   */
+  void VisitWithinRange(const GeoPoint &location, double range,
+                        AirspaceVisitor &visitor,
+                        const AirspacePredicate &predicate =
+                              AirspacePredicate::always_true) const;
 
   gcc_pure
   const_iterator_range QueryAll() const {

src/Engine/Airspace/Minimum.hpp
@@ -25,6 +25,7 @@ Copyright_License {
 #define AIRSPACE_MINIMUM_HPP
 
 #include "Airspaces.hpp"
+#include "AirspaceVisitor.hpp"
 
 template<class Func,
          typename Result=decltype(((Func *)nullptr)->operator()(*(const AbstractAirspace *)nullptr)),
@@ -36,15 +37,22 @@ FindMinimum(const Airspaces &airspaces, const GeoPoint &location, double range,
             Func &&func,
             Cmp &&cmp=Cmp())
 {
-  Result minimum;
-  for (const auto &i : airspaces.QueryWithinRange(location, range)) {
-    const AbstractAirspace &aa = i.GetAirspace();
-    Result result = func(aa);
-    if (cmp(result, minimum))
-      minimum = result;
-  }
+  struct FindMinimumVisitor final : AirspaceVisitor, Func, Cmp {
+    Result minimum;
 
-  return minimum;
+    FindMinimumVisitor(Func &&_func, Cmp &&_cmp)
+      :Func(std::move(_func)), Cmp(std::move(_cmp)) {}
+
+    void Visit(const AbstractAirspace &aa) override {
+      Result result = Func::operator()(aa);
+      if (Cmp::operator()(result, minimum))
+        minimum = result;
+    }
+  };
+
+  FindMinimumVisitor visitor(std::move(func), std::move(cmp));
+  airspaces.VisitWithinRange(location, range, visitor, predicate);
+  return visitor.minimum;
 }
 
 #endif