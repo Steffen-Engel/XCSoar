06f43969adaf1c783817b58d9fb3cb8fbdcd1039
Steffen Engel <steffen.engel@nutrimat.de>
2023-10-25 18:52:09 +0200

    adjust menu colors

--- src/Look/Colors.hpp
+++ src/Look/Colors.hpp
@@ -6,9 +6,9 @@
 #include "ui/canvas/Color.hpp"
 
 #ifdef XCSOAR_TESTING
-static constexpr Color COLOR_XCSOAR_LIGHT = Color(0xff, 0xef, 0x9d);
-static constexpr Color COLOR_XCSOAR = Color(0xd8, 0x94, 0x00);
-static constexpr Color COLOR_XCSOAR_DARK = Color(0x35, 0x00, 0x35);
+static constexpr Color COLOR_XCSOAR_LIGHT = Color(168, 0, 168);
+static constexpr Color COLOR_XCSOAR = Color(128, 0, 128);
+static constexpr Color COLOR_XCSOAR_DARK = Color(100, 0, 100);
 #else
 static constexpr Color COLOR_XCSOAR_LIGHT = Color(0xaa, 0xc9, 0xe4);
 static constexpr Color COLOR_XCSOAR = Color(0x3f, 0x76, 0xa8);
