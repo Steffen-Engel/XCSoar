26c8a5f36fd132147547863b4a97d5565742a1c4
Steffen-Engel <steffen.engel@nutrimat.de>
2026-03-30 15:37:21 +0100

    merge step 2

--- src/InfoBoxes/Content/Airspace.hpp
+++ src/InfoBoxes/Content/Airspace.hpp
@@ -7,6 +7,27 @@
 
 struct InfoBoxData;
 
+class InfoBoxContentAirspaces_Distance : public InfoBoxContent
+{
+ public:
+  virtual void Update(InfoBoxData &data) noexcept override;
+  virtual void OnCustomPaint(Canvas &canvas, const PixelRect &rc) noexcept override;
+};
+
+class InfoBoxContentAirspaces_Name : public InfoBoxContent
+{
+ public:
+  virtual void Update(InfoBoxData &data) noexcept override;
+  virtual void OnCustomPaint(Canvas &canvas, const PixelRect &rc) noexcept override;
+};
+
+class InfoBoxContentAirspaces_Altitude : public InfoBoxContent
+{
+ public:
+  virtual void Update(InfoBoxData &data) noexcept override;
+  virtual void OnCustomPaint(Canvas &canvas, const PixelRect &rc) noexcept override;
+};
+
 
 class InfoBoxNearestAirspaceHorizontal : public InfoBoxContent
 {
